f670b703b93434c5c87c5a9249b20ba428c5d460
Ian Mori <imori@hollandcollege.com>
2015-02-18 14:49:03 +0300

    added overflow

--- SoMeme/app/src/main/java/com/example/jon/someme/Activities/MainActivity.java
+++ SoMeme/app/src/main/java/com/example/jon/someme/Activities/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.jon.someme.R;
 
@@ -101,25 +102,54 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        super.onOptionsItemSelected(item);
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        super.onOptionsItemSelected(item);
+
+        Intent i;
+        switch(item.getItemId()){
+
+            case R.id.favorites:
+                i = new Intent(getApplicationContext(), FavoriteActivity.class);
+                startActivity(i);
+                //Toast.makeText(getBaseContext(), "You selected favorites", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.profile:
+                i = new Intent(getApplicationContext(), UserProfileActivity.class);
+                startActivity(i);
+                //Toast.makeText(getBaseContext(), "You selected profile", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.logout:
+                //i = new Intent(getApplicationContext(), UserActivity.class);
+                //startActivity(i);
+                Toast.makeText(getBaseContext(), "You selected logout", Toast.LENGTH_SHORT).show();
+                break;
         }
+        return true;
 
-        return super.onOptionsItemSelected(item);
     }
-
-
-
 
 }
