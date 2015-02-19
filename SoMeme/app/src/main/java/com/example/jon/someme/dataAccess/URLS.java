f702810c25ceaf8698cac88c1849792ac352ee76
Ryan Forrester <ryan.forrester13@gmail.com>
2015-02-19 09:12:02 -0400

    My URL needs merging

--- SoMeme/app/src/main/java/com/example/jon/someme/dataAccess/URLS.java
+++ SoMeme/app/src/main/java/com/example/jon/someme/dataAccess/URLS.java
@@ -4,7 +4,7 @@ package com.example.jon.someme.dataAccess;
  * Created by Jon on 2/15/2015.
  */
 public class URLS {
-    public static final String host = "192.168.0.12:80";
+    public static final String host = "192.168.2.13:80";
 
     public static final String memeList = "http://"+host+"/SoMeme/data/memeListData.php";
     public static final String memeView = "http://"+host+"/SoMeme/data/memeViewData.php";
